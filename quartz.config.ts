0a9b2782322615dde1beea208f7719beaf0cba78
PixelMelt <44953835+PixelMelt@users.noreply.github.com>
2024-11-28 01:44:14 -0500

    Quartz sync: Nov 28, 2024, 1:44 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Pix's blog",
-    pageTitleSuffix: " - CWPT blog",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
@@ -17,7 +17,12 @@ const config: QuartzConfig = {
     baseUrl: "blog.pixelmelt.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
-    generateSocialImages: false,
+    generateSocialImages: {
+      colorScheme: "darkMode", // what colors to use for generating image, same as theme colors from config, valid values are "darkMode" and "lightMode"
+      width: 1200, // width to generate with (in pixels)
+      height: 630, // height to generate with (in pixels)
+      excludeRoot: false, // wether to exclude "/" index path to be excluded from auto generated images (false = use auto, true = use default og image)
+    },
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: false,
